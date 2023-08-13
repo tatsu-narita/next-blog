8187594e1c929e1970d4eeb4580f4c857f65b629
tatsu-narita <nt0604.b@gmail.com>
2023-08-14 02:10:10 +0900

    add: ファイルの追加とimport
    ・header
    ・hero
    ・footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
 
 export default function Home() {
-  const subtitle = 'アウトプットしていくサイト'
   return (
-    <div className= "hero">
-      <h1 style={{ color: 'red', fontSize: '80px' }}>
-        CUBE</h1>
-      <p>{subtitle}</p>
-    </div>
+    <>
+    <Header />
+
+    <main>
+      <Hero />
+    </main>
+    
+    <Footer />
+    </>
   )
 }
