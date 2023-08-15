d6c7ade88766adeaf7c781f8c4d900a3a35b5d45
tatsu-narita <nt0604.b@gmail.com>
2023-08-15 23:56:55 +0900

    add:ファイルの追加とその他ファイルの修正
    追加:_app.tsx
    修正:components/layout.tsx, app/page.tsx,

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import Image from 'next/image'
-import Header from '../components/header'
-import Hero from '../components/hero'
-import Footer from '../components/footer'
+import Hero from 'components/hero'
 
 export default function Home() {
-  return (
-    <>
-    <Header />
-
-    <main>
-      <Hero />
-    </main>
-    
-    <Footer />
-    </>
-  )
+  return <Hero />
 }
