fc037053c656afaf652ad49f00777f323a138d32
tatsu-narita <nt0604.b@gmail.com>
2023-09-07 23:48:12 +0900

    update: aboutページに画像表示
    [about/page.tsx]
    ・画像表示
    ・優先的に読み込み
    ・blur画像
    [globals.css]
    ・transition

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,23 @@ import Hero from 'components/hero'
 import PostBody from 'components/post-body'
 import Contact from 'components/contact'
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import Image from 'next/legacy/image'
+import eyecatch from 'images/about.jpg'
 
 export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
+
+<figure>
+  <Image
+    src={eyecatch}
+    alt="" layout="responsive"
+    sizes="(min-width: 1152px) 1152px, 100vw"
+    priority
+    placeholder="blur"
+  />
+</figure>
 
       <TwoColumn>
         <TwoColumnMain>
