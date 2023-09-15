25e55e5a70da5edc56b53c279838302a3070d03d
tatsu-narita <nt0604.b@gmail.com>
2023-09-15 23:48:52 +0900

    update: Metaデータの追加
    ・about/page.tsx
    ・blog/page.tsx
    をそれぞれ修正

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,14 +4,19 @@ import Head from "next/head";
 import {siteMeta} from 'lib/constants'
 const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
 
-export default function Meta({ pageTitle }) {
+export default function Meta({ pageTitle, pageDesc }) {
     //ページのタイトル
     const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
+    //ページの説明
+    const desc = pageDesc ?? siteDesc
 
     return (
         <Head>
             <title>{title}</title>
             <meta property="og:title" content={title} />
+
+            <mata name="description" content={desc} />
+            <meta property="og:description" content={desc} />
         </Head>
     )
 }
