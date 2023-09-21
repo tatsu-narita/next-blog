086ac52607dc5884b861c6c3eb9ae9b36a7397cd
tatsu-narita <nt0604.b@gmail.com>
2023-09-21 23:57:41 +0900

    add: 非同期処理
    setTime()、Promiseオブジェクトで処理

--- app/blog/schedule.tsx
+++ app/blog/schedule.tsx
@@ -1,10 +1,12 @@
+import { client } from 'lib/api'
+
 export default function schedule() {
     return <h1>記事のタイトル</h1>
 }
 
 export async function getstaticProps() {
     console.log('処理1')
-    console.log('処理2')
+    setTimeout(() => console.log('処理2'), 1000)
     console.log('処理3')
 
     return {
