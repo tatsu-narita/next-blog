ad3227d8e532ddc4d5d15c57c6ea1c4373f1efbf
tatsu-narita <nt0604.b@gmail.com>
2023-09-22 23:55:30 +0900

    update: 非同期処理
    Promiseオブジェクト

--- app/blog/schedule.tsx
+++ app/blog/schedule.tsx
@@ -4,7 +4,12 @@ export default function schedule() {
     return <h1>記事のタイトル</h1>
 }
 
-export async function getstaticProps() {
+export async function getStaticProps() {
+    const resPromise = client.get({
+        endpoint: 'blogs'
+    })
+    console.log(resPromise)
+
     console.log('処理1')
     setTimeout(() => console.log('処理2'), 1000)
     console.log('処理3')
