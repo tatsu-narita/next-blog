303d23eed80591ff398714d6b49bc95ff28e6a7b
tatsu-narita <nt0604.b@gmail.com>
2023-09-23 00:10:54 +0900

    update: async/await
    await式を使う

--- app/blog/schedule.tsx
+++ app/blog/schedule.tsx
@@ -11,6 +11,13 @@ export async function getStaticProps() {
     console.log(resPromise)
     resPromise.then((res) => console.log(res)).catch((err) => console.log(err))
 
+    try{
+        const res = await resPromise
+        console.log(res)
+    } catch (err) {
+        console.log(err)
+    }
+
     console.log('処理1')
     setTimeout(() => console.log('処理2'), 1000)
     console.log('処理3')
