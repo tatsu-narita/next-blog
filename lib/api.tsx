8f2b5791994efe974860c8c8cbec160b68998254
tatsu-narita <nt0604.b@gmail.com>
2023-09-23 23:43:28 +0900

    update: 記事ページに必要なデータを取得
    ・getPostBySlug作成

--- lib/api.tsx
+++ lib/api.tsx
@@ -4,3 +4,16 @@ export const client = createClient({
     serviceDomain: process.env.SERVICE_DOMAIN,
     apikey: process.env.API_KEY,
 })
+
+export async function getPostBySlug(sulg) {
+    tly {
+        const post = await client.get({
+            endpoint: 'blogs'
+            queries: { filters: 'slug[equals]${slug}' },
+        })
+        return post.contents[0]
+    } catch (err) {
+        console.log('~~ getPostBySlug ~~')
+        console.log(err)
+    }
+}
