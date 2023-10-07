ac3ffc1dbf00272190276cd371ec33cef22c2121
tatsu-narita <nt0604.b@gmail.com>
2023-10-07 23:56:47 +0900

    update: カテゴリー
    カテゴリーをリストにする

--- components/post-categories.tsx
+++ components/post-categories.tsx
@@ -1,2 +1,16 @@
 import styles from 'styles/post-categories.module.css'
 import Link from 'next/link'
+
+export default function PostCategories({ categories }) {
+    return (
+        <ul className={styles.list}>
+            {categories.map(({ name, slug }) => (
+                <li key={slug}>
+                    <link href={'/blog/category/${slug}'}>
+                        <a>{name}</a>
+                    </link>
+                </li>
+            ))}
+        </ul>
+    )
+}
